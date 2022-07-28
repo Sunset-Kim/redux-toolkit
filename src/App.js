904626b21f874f3da942f71140735d378f5c48e2
Sunset-kim <wood2002@naver.com>
2022-07-28 23:09:58 +0900

    기본적인 Redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 function App() {
-  return <div className="App"></div>;
+  const { value } = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+  console.log(value);
+
+  const increaseCounter = useCallback(() => dispatch({ type: "increase" }), []);
+
+  return (
+    <div className="App">
+      <div>
+        <div>
+          <p>카운트</p>
+          <p>{value}</p>
+        </div>
+        <button onClick={increaseCounter}>증가</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import App from "./App";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increase":
+      return { ...state, count: { value: state.count.value + 1 } };
+
+    default:
+      return state;
+  }
+}
+
+const initialState = {
+  count: {
+    value: 0,
+  },
+};
+
+const store = createStore(reducer, initialState);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
